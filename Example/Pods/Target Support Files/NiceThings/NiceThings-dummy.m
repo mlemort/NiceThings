85bb3dcebff34bc87357764d5ff8e6c92a3dcd18
maxime lemort <maxime.lemort@technosens.fr>
2020-09-16 16:05:58 +0200

    run pod lib create NiceThings

---
+++ Example/Pods/Target Support Files/NiceThings/NiceThings-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NiceThings : NSObject
+@end
+@implementation PodsDummy_NiceThings
+@end
---
+++ Example/Pods/Target Support Files/Pods-NiceThings_Example/Pods-NiceThings_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NiceThings_Example : NSObject
+@end
+@implementation PodsDummy_Pods_NiceThings_Example
+@end
