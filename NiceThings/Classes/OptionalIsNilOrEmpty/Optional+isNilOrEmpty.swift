1aa77eb443d8f3fbafe799aa2075b378f055a100
Pierre-EdouardLieb <plieb@student.42.fr>
2020-09-17 17:17:05 +0200

    Update Optional+isNilOrEmpty.swift

--- NiceThings/Classes/OptionalIsNilOrEmpty/Optional+isNilOrEmpty.swift
+++ NiceThings/Classes/OptionalIsNilOrEmpty/Optional+isNilOrEmpty.swift
@@ -10,7 +10,7 @@ import Foundation
 public extension Optional where Wrapped: Collection {
 
     /**
-     Boolean value indicate if the optional is nil or if the collection wrapped is empty
+     Boolean value indicates if the optional is nil or if the collection wrapped is empty
     */
     var isNilOrEmpty: Bool {
         return self?.isEmpty ?? true
